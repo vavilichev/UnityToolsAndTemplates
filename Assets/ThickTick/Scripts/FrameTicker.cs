d44b3ec685f4070620e784c417e32d991b00cd92
vavilichev <andreyvavilichev@gmail.com>
2024-12-19 01:24:35 +0400

    ThickTick

---
+++ Assets/ThickTick/Scripts/FrameTicker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Vavilichev.UTAT.ThickTick
+{
+    public class FrameTicker : MonoBehaviour
+    {
+        public event Action<float> OnTick;
+
+        private bool _isUnscaledTime;
+
+        public static FrameTicker Create(bool isUnscaledTime = false)
+        {
+            var mono = new GameObject("[TICKER]");
+            var ticker = mono.AddComponent<FrameTicker>();
+            ticker._isUnscaledTime = isUnscaledTime;
+
+            return ticker;
+        }
+        
+        private void Update()
+        {
+            var dt = _isUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+            OnTick?.Invoke(dt);
+        }
+    }
+}
